58643d0a7cb3cf3eeeb10fac872f2a3833f38e72
thesexydev <37409603+thesexydev@users.noreply.github.com>
2018-03-15 18:28:19 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -39,6 +39,35 @@ var app = new Vue({
       this.showSettings = false;
       this.showGame = true;
       this.showInstructions = false;
+    },
+
+    getPlayersData() {
+      if (window.localStorage.getItem("player1")) {
+        this.player1 = window.localStorage.getItem("player1");
+      }
+
+      if (window.localStorage.getItem("player2")) {
+        this.player2 = window.localStorage.getItem("player2");
+      }
+    },
+    savePlayersData() {
+      console.log("savePlayersData");
+      if (this.player1) {
+        console.log("this.player1", this.player1);
+
+        window.localStorage.setItem("player1", this.player1);
+      }
+      if (this.player2) {
+        console.log("this.player2", this.player2);
+        window.localStorage.setItem("player2", this.player2);
+      }
+    },
+    saveSettings() {
+      this.savePlayersData();
+      this.closeSettings();
     }
+  },
+  mounted() {
+    this.getPlayersData();
   }
 });
